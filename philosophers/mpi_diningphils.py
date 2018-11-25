0327c1d2cfcae0ed5032283f07813a03651055b0
Namratha Basavanahalli Manjunatha <namratha@ruby.cecs.pdx.edu>
2018-11-25 01:42:24 -0800

    working dining philosophers with MPI. More testing in progress.

--- philosophers/mpi_diningphils.py
+++ philosophers/mpi_diningphils.py
@@ -1,225 +1,196 @@
-import socket
+from mpi4py import MPI
 import pickle
 from threading import Thread, Lock
 import time
-from _thread import *
+from thread import *
 import random
 
-
-program_duration = 30
-philosophers = []
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
 lock = Lock()
 
+program_duration = 30
+philosopher = None
+chopsticks = []
+
 # Client represents the 5 philosophers
-if rank=0
+if rank!=5:
     class Philosopher:
-        total_wait_time = 0
-        times_eating = 0
 
         def __init__(self, name, left_chopstick, right_chopstick):
             self.name = name
             self.chopstick1 = left_chopstick
             self.chopstick2 = right_chopstick
 
-
     class Client(Thread):
         running = True
+	total_wait_time = 0
+	times_eating = 0
+	philosopher_name = None
 
         def run(self):
-            host = '127.0.0.1'
-            port = 12345
+	    self.philosopher_name = "Philosopher"+str(rank+1)
+	    left_chopstick = rank
+	    if rank == 4:
+	        right_chopstick = 0
+	    else:
+		right_chopstick = rank+1
+	    philosopher = Philosopher(self.philosopher_name, left_chopstick, right_chopstick)
+            start_new_thread(self.attempt_to_eat, (philosopher,))
 
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((host, port))
-            for i in range(5):
-                start_new_thread(self.attempt_to_eat, (s, philosophers[i]))
-
-        def attempt_to_eat(self, s, philosopher):
+        def attempt_to_eat(self, philosopher):
             chopstick1 = philosopher.chopstick1
             chopstick2 = philosopher.chopstick2
 
             while self.running:
                 recv_data = -1
                 t0 = time.time()
-                print(philosopher.name, "is waiting...")
+                print(self.philosopher_name+" - is waiting...")
                 while recv_data == -1:
                     send_data = []
                     sticks_to_request = []
                     send_data.append("request")
-                    send_data.append(philosopher.name)
+                    send_data.append(self.philosopher_name)
                     sticks_to_request.append(chopstick1)
                     send_data.append(sticks_to_request)
                     data = pickle.dumps(send_data)
                     try:
                         lock.acquire()
-                        s.send(data)
-                        lock.release()
-                    except:
-                        pass
+                        data = comm.bcast(data,root=rank)
+                        #comm.send(send_data,dest=5,tag=11)
+			lock.release()
+                    except Exception as e:
+			pass
 
                     try:
-                        r_data = s.recv(1024)
+                        r_data = comm.bcast(data,root=rank)
+                        #recv_data = comm.recv(source=MPI.ANY_SOURCE, tag=11)
                         recv_data = pickle.loads(r_data)
-                    except:
+                    except Exception:
                         pass
 
                 t1 = time.time()
-                philosopher.total_wait_time += (t1 - t0)
+                self.total_wait_time += (t1 - t0)
 
-                print(philosopher.name, "picks up chopstick 1, attempts to pick up chopstick 2")
+                print(self.philosopher_name+" - picks up chopstick 1, attempts to pick up chopstick 2")
                 send_data = []
                 sticks_to_request = []
                 send_data.append("request")
-                send_data.append(philosopher.name)
+                send_data.append(self.philosopher_name)
                 sticks_to_request.append(chopstick2)
                 send_data.append(sticks_to_request)
                 data = pickle.dumps(send_data)
                 try:
                     lock.acquire()
-                    s.send(data)
+                    data = comm.bcast(data,root=rank)
                     lock.release()
                 except:
                     pass
 
                 try:
-                    r_data = s.recv(1024)
+                    r_data = comm.bcast(data,root=rank)
                     recv_data = pickle.loads(r_data)
                 except:
                     pass
 
                 send_data = []
                 if recv_data == -1:
-                    print(philosopher.name, "failed to pick up chopstick 2, drops chopstick 1")
+                    print(self.philosopher_name, "failed to pick up chopstick 2, drops chopstick 1")
                     send_data.append("release")
                     sticks_to_release = []
-                    send_data.append(philosopher.name)
+                    send_data.append(self.philosopher_name)
                     sticks_to_release.append(chopstick1)
                     send_data.append(sticks_to_release)
                     data = pickle.dumps(send_data)
                     try:
                         lock.acquire()
-                        s.send(data)
+                        data = comm.bcast(data,root=rank)
                         lock.release()
                     except:
                         pass
 
                     try:
-                        s.recv(1024)
+                        r_data = comm.bcast(data,root=rank)
                     except:
                         pass
 
                     time.sleep(random.random())
                 else:
                     sticks_to_release = []
-                    print(philosopher.name, "has both chopsticks!")
+                    print(self.philosopher_name+" - has both chopsticks!")
                     self.eat(philosopher)
                     send_data.append("release")
-                    send_data.append(philosopher.name)
+                    send_data.append(self.philosopher_name)
                     sticks_to_release.append(chopstick1)
                     sticks_to_release.append(chopstick2)
                     send_data.append(sticks_to_release)
                     data = pickle.dumps(send_data)
                     try:
                         lock.acquire()
-                        s.send(data)
+                        data = comm.bcast(data,root=rank)
                         lock.release()
                     except:
                         pass
 
                     try:
-                        s.recv(1024)
+                        r_data = comm.bcast(data,root=rank)
                     except:
                         pass
 
                     time.sleep(random.random())
 
-            s.close()
-
         def eat(self, philosopher):
-            print(philosopher.name, "starts eating")
-            philosopher.times_eating += 1
+            print(self.philosopher_name+" - starts eating")
+            self.times_eating += 1
             time.sleep(random.random())
-            print(philosopher.name, "finishes eating, drops both chopsticks")
+            print(self.philosopher_name+" - finishes eating, drops both chopsticks")
 
-
-    def create_philosophers():
-        chopsticks = []
-        for i in range(5):
-            chopsticks.append(i)
-
-        philosopher_names = ['Philosopher 1', 'Philosopher 2', 'Philosopher 3', 'Philosopher 4', 'Philosopher 5']
-
-        for i in range(5):
-            left_chopstick = chopsticks[i]
-            if i == 4:
-                right_chopstick = chopsticks[0]
-            else:
-                right_chopstick = chopsticks[i + 1]
-            philosopher = Philosopher(philosopher_names[i], left_chopstick, right_chopstick)
-            philosophers.append(philosopher)
-
-
-    create_philosophers()
     client = Client()
     client.start()
     time.sleep(program_duration)
     client.running = False
 
-    print("Finishing...")
-    print("\nWaiting times:")
-    for philosopher in philosophers:
-        print(philosopher.name, ": ", philosopher.total_wait_time)
+    print("\nPhilosopher %d - Finishing..."%(rank+1))
+    print("Philosopher %d - Waiting times: "%(rank+1), client.total_wait_time)
+    print("Philosopher %d - Number of times eating: "%(rank+1),client.times_eating)
+    time.sleep(2)
 
-    print("\nNumber of times eating:")
-    for philosopher in philosophers:
-        print(philosopher.name, ": ", philosopher.times_eating)
 # Server acts as the "waiter" in the dining philosophers problem.
 # Server manages the distribution of the chopsticks to the philosophers
-else:
+elif rank == 5:
     class Server(Thread):
         running = True
 
         def run(self):
-            host = "127.0.0.1"
-            port = 12345
-
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.bind((host, port))
-
-            print("socket bound to port", port)
-            s.listen(5)
-
-            c, addr = s.accept()
-
             while self.running:
-                r_data = c.recv(1024)
-                try:
+		data = None
+                r_data = comm.bcast(data,root=rank)
+                
+		try:
                     recvd_data = pickle.loads(r_data)
                 except EOFError:
                     print("ending program")
                     break
+		except Exception as e:
+		    continue
                 action = recvd_data[0]
                 philosopher = recvd_data[1]
                 sticks = recvd_data[2]
 
                 if action == "request":
                     for j in range(len(sticks)):
-                        print(philosopher, "requested chopstick", sticks[j])
+                        print(philosopher+" - requested chopstick"+str(sticks[j]))
                         send_data = self.request(sticks[j])
                         s_data = pickle.dumps(send_data)
-                        print('Sending', philosopher, ': ', send_data)
-                        c.send(s_data)
+                        print('Sending '+philosopher+': '+str(send_data))
+                        data = comm.bcast(s_data,root=5)
                 elif action == "release":
                     for j in range(len(sticks)):
-                        print(philosopher, "releases chopstick", sticks[j])
+                        print(philosopher+" - releases chopstick"+str(sticks[j]))
                         self.release(sticks[j])
                     send_data = "all done"
                     s_data = pickle.dumps(send_data)
-                    print('Sending', philosopher, ': ', send_data)
-                    c.send(s_data)
-
-            c.close()
-            s.close()
+                    data = comm.bcast(s_data,root=5)
 
         def request(self, chopstick_id):
             for chopstick in chopsticks:
@@ -239,4 +210,4 @@ else:
     server.start()
     time.sleep(program_duration)
     server.running = False
-    print("Finishing...")
+    print("Waiter Finishing...")
